679b8fc9df76373c7f118f2db8e194edb765063c
aparnacl <18494870+aparnacl@users.noreply.github.com>
2021-09-26 18:39:42 +0530

    Add files via upload

---
+++ assets/data/handler.js
@@ -0,0 +1,487 @@
+const products = [
+    {
+        id: "bullet-3-2.4MP",
+        name: "Bullet 2.4MP",
+        description: "2.4MP Full HD IR Cosmic Bullet Camera - 20Mtr.",
+        price: 2300,
+        features: [
+            ["Image Sensor", `1/2.7" 2.4MP PS CMOS Image Sensor (6.858 centimeters)`],
+            ["Minimum Illumination", `0.02Lux/F1.85, 30IRE, 0Lux IR on`],
+            ["Effective Pixels", `2.4MP`]
+        ],
+        bulletPoints: [
+            `1/2.7" 2.4MP PS CMOS Image Sensor (6.858 centimeters)`,
+            `Max 25/30fps@ 2.4MP`,
+            `DWDR, Day/Night(ICR), 2D-DNR, AWB, AGC, BLC, HLC`,
+            `3.6mm fixed lens (2.8mm, 6mm optional)`,
+            `IR Range of 20 Mtrs., IP67`
+        ],
+        imageLink: "https://www.cpplusworld.com/prodassets/product/small/cp-usc-ta24l2.jpg",
+        category: "cc"
+    },
+
+    {
+        id: "dome",
+        name: "Dome 2.4MP",
+        description: "2.4MP Full HD IR Cosmic Dome Camera - 30Mtr",
+        price: 2100,
+        features: [
+            ["Image Sensor", `1/2.7" 2.4MP PS CMOS Image Sensor (6.858 centimeters)`],
+            ["Minimum Illumination", `0.02Lux/F1.85, 30IRE, 0Lux IR on`],
+            ["Effective Pixels", `2.4MP`],
+            ["BodyType", `Dome`]
+
+        ],
+        bulletPoints: [
+            `1/2.7" 2.4MP PS CMOS Image Sensor (6.858 centimeters)`,
+            `Max 25/30fps@ 2.4MP`,
+            `DWDR, Day/Night(ICR), 2D-DNR, AWB, AGC, BLC, HLC`,
+            `3.6mm fixed lens (2.8mm, 6mm optional)`,
+            `IR Range of 20 Mtrs., IP67`,
+            `HD, SD and Two other Analog format Output switchable`
+        ],
+        imageLink: "https://www.cpplusworld.com/prodassets/product/small/70bb461a-f28d-47c5-8107-2a0dd7ab7218.png",
+        category: "cc"
+    },
+
+    {
+        id: "c11",
+        name: "Beetel C-11 Landline Basic Phone (Black)",
+        description: "Brand:Beetel",
+        price: 550,
+        features: [
+            ["Colour", `BLACK`],
+            ["What's in the box", `PHONE, RJ11 CORD, SPIRAL CORD, WARRANTY CARD`],
+            ["Manufacturer", `BEETEL TELETECH LTD`],
+            ["Country of Origin", `INDIA`]
+
+        ],
+        bulletPoints: [
+            `Item Weight: 150g`,
+            `Product Dimensions: 21.3 x 19.3 x 6.4 cm`,
+            `Colour: black`,
+        ],
+        imageLink: "https://m.media-amazon.com/images/I/31AVBNi5YKL.jpg",
+        category: "te"
+    },
+
+    {
+        id: "m78",
+        name: "Beetel M78 Corded Landline Phone  (Black)",
+        description: "Brand:Beetel",
+        price: 000,
+        features: [
+            ["Keypad", `Alphanumerical with Multi-functional Keys`],
+            ["Dedicated Buttons", `Flash Buttons`],
+            ["Manufacturer", `BEETEL TELETECH LTD`],
+            ["Country of Origin", `INDIA`],
+            
+
+        ],
+        bulletPoints: [
+            `Type:       Corded`,
+            `Model Name: M78`,
+            `Display:    Alphanumeric Display`
+            
+        ],
+        imageLink: "https://rukminim1.flixcart.com/image/832/832/landline-phone/g/e/z/beetel-m78-original-imadcyg9cwgbsgzq.jpeg?q=70",
+        category: "te"
+    },
+
+    {
+        id: "b25",
+        name: "Beetel B25 Corded Landline Phone  (Black)",
+        description: "Brand:Beetel",
+        price: 000,
+        features: [
+            ["Description", `
+            Beetel presents this corded landline phone that is smart, compact, and packed with utility features that make it an ideal device for use at home or office.
+            
+            Ringer Volume Control
+            
+            You can adjust the ringer to operate at a high or low volume according to your preference.
+            
+            Wall Mountable
+            
+            You can either place this phone on a tabletop or mount it on a wall if you have space limitations.
+            
+            Other Features
+            
+            There are separate buttons for features such as flash, pause, and redial. You can switch between an ongoing and a waiting call by pressing the flash button, press the pause button before affixing a code to an inter-city number for a quick connection, and also call the last dialed number simply by pressing the redial button.`],
+            
+            
+
+        ],
+        bulletPoints: [
+           `Type: CORDED`,
+           `Model Name: B25`
+            
+        ],
+        imageLink: "https://rukminim1.flixcart.com/image/832/832/kl175ow0/landline-phone/f/1/x/b25-beetel-original-imagy8x6snhn77zu.jpeg?q=70",
+        category: "te"
+    },
+
+    {
+        id: "m71",
+        name: "Beetel M71 corded landline phone white",
+        description: "Brand:Beetel",
+        price: 000,
+        features: [
+            ["Product Dimensions", `‎‎‎20.5 x 9.8 x 12.8 cm; 510 Grams`],
+            ["Type", `Corded`],
+            ["Color",`White`],
+            ["Material", `Plastic`],
+            ["Display", `16 digit LCD Display`],
+            ["Manufacturer", `BEETEL TELETECH LIMITED`]
+            
+
+        ],
+        bulletPoints: [
+            `3 AA batteries required`,
+            `6 one touch memory`,
+            `99 incoming calls`,
+            `18 outgoing calls`,
+            `Speaker`,
+            `Pre dial and edit function`
+            
+        ],
+        imageLink: "https://m.media-amazon.com/images/I/514czZYgStL._SL1100_.jpg",
+        category: "te"
+    },
+
+    {
+        id: "m59",
+        name: "Beetel M-59 Corded Landline Caller ID phone (Colour - white)",
+        description: "Brand:Beetel",
+        price: 000,
+        features: [
+            ["Product Dimensions", `20 x 16.2 x 8.5 Centimeters`],
+            ["Type", `Corded`],
+            ["Manufacturer", `BEETEL TELETECH LTD`],
+            ["Country of Origin", `INDIA`],
+            
+
+        ],
+        bulletPoints: [
+            `16 digit backlit LCD display `,
+            `10 one touch memory`,
+            `37 incoming calls memory`,
+            `15 outgoing calls memory`,
+            `2 way speaker phone`,
+            `Programmable flash`,
+            `Music on hold`
+            
+        ],
+        imageLink: "https://m.media-amazon.com/images/I/71Kj3J-zfxL._SL1500_.jpg",
+        category: "te"
+    },
+
+    {
+        id: "m51",
+        name: "Beetel M51 Corded Landline Phone  (Black)",
+        description: "Brand:Beetel",
+        price: 000,
+        features: [
+            ["Product Description", `Talk for hours on end with your own Beetel M51 Corded Landline. You can feel more at home with this simplistic , 3-line LCD display landline. You can store up to 30 incoming calls and 5 outgoing calls.`],
+            ["LCD Display", `This corded landline has a 3-line display which shows you the caller's number, time and day.`],
+            ["Manufacturer", `BEETEL TELETECH LTD`],
+            ["Country of Origin", `INDIA`],
+            
+
+        ],
+        bulletPoints: [
+            `3 Line Displayx `,
+            `10 one touch memory`,
+            `30 incoming calls memory`,
+            `5 outgoing calls memory`
+            
+            
+        ],
+        imageLink: "https://rukminim1.flixcart.com/image/832/832/landline-phone/p/v/x/beetel-m51-original-imad8eugkzsphgqy.jpeg?q=70",
+        category: "te"
+    },
+
+    {
+        id: "x70",
+        name: "BEETEL X-70 BLACK CORDLESS PHONE",
+        description: "Brand:Beetel",
+        price: 000,
+        features: [
+            ["Product Dimensions", `‎7.2 x 9.5 x 18 cm; 240 Grams`],
+            ["Type", `Cordless`],
+            ["Standing screen display size",`1.67 inches`],
+            ["Manufacturer", `BEETEL TELETECH LTD`],
+            ["Country of Origin", `INDIA`],
+            
+
+        ],
+        bulletPoints: [
+            `2 AAA Batteries required. (included) `,
+            `50 Nos phone book memory`,
+            `20 incoming calls memory`,
+            `15 outgoing calls memory`,
+            `Two-way speaker phone`,
+            `Mute Function`
+            
+            
+        ],
+        imageLink: "https://m.media-amazon.com/images/I/71-HlcxTAeL._SL1500_.jpg",
+        category: "te"
+    },
+
+    {
+        id: "x73",
+        name: "Beetel X73 Cordless Phone Black",
+        description: "Brand:Beetel",
+        price: 000,
+        features: [
+            ["Product Dimensions", `‎‎17 x 12.5 x 8.8 cm; 400 Grams`],
+            ["Type", `Cordless`],
+            ["Standing screen display size",`1.67 inches`],
+            ["Manufacturer", `BEETEL TELETECH LTD`],
+            ["Country of Origin", `INDIA`],
+            
+
+        ],
+        bulletPoints: [
+            `2 AAA Batteries required.`,
+            `Battery Powered`,
+            
+        ],
+        imageLink: "https://m.media-amazon.com/images/I/61cIvABtr1L._SL1028_.jpg",
+        category: "te"
+    },
+
+    {
+        id: "p3611",
+        name: "Panasonic KX-TG3612BX Digital Cordless Phone (Black)(3611)",
+        description: "Brand:Panasonic",
+        price: 000,
+        features: [
+            ["Product Dimensions", `‎‎17 x 12.5 x 8.8 cm; 400 Grams`],
+            ["Type", `Caller id cordless phone`],
+            ["2.4GHz",`Frequency support`],
+            ["Battery Life", `AAA NI-MH battery providing talk-time of 5 hours and standby time of 264 hours`],
+            ["Manufacturer", `Panasonic`],
+            
+
+        ],
+        bulletPoints: [
+            `1 Lithium Polymer batteries required (included)`,
+            `Power Source: Battery`,
+            `50 Phonebook entries`,
+            `5 redial memories`
+            
+        ],
+        imageLink: "https://m.media-amazon.com/images/I/611jJb66OsL._SL1500_.jpg",
+        category: "te"
+    },
+
+    {
+        id: "p3711",
+        name: "Panasonic Single Line Digital Cordless Telephone, Black (3711)",
+        description: "Brand:Panasonic",
+        price: 000,
+        features: [
+            ["Product Dimensions", `‎‎‎20.5 x 9.8 x 12.8 cm; 510 Grams`],
+            ["Type", `Cordless`],
+            ["Color",`Black`],
+            ["Battery Life", `AAA NI-MH battery providing talk-time of 5 hours and standby time of 264 hours`],
+            ["Display", `1.8-inch LCD Display`],
+            ["Manufacturer", `Panasonic`],
+            ["Box Includes", `Handset, Base, Battery, Charger and User Guide`]
+            
+
+        ],
+        bulletPoints: [
+            `1 Lithium Polymer batteries required(include)`,
+            `Power Source: Corded Electric`,
+            `50 Phonebook entries`,
+            `5 redial memories`,
+            `Speaker`
+            
+        ],
+        imageLink: "https://media.croma.com/image/upload/f_auto,q_auto,d_Croma%20Assets:no-product-image.jpg,h_350,w_350/v1605268286/Croma%20Assets/Communication/Landline/Images/8996938186782.png",
+        category: "te"
+    },
+    {
+        id: "c51",
+        name: "Beetel C51 Corded Landline Phone (Black)",
+        description: "Brand:Beetel",
+        price: 000,
+        features: [
+            ["Product Dimensions", `‎‎‎13 x 20 x 8 cm; 542 Grams`],
+            ["Type", `Corded`],
+            ["Color",`Black`],
+            ["Material", `Warranty card and Retailer bill, One Coil cord, One base unit, One handset, One main line cord`],
+            ["Set includes",`one base unit, one handset, one main line cord and one coil cord`],
+            ["Power Source", `Battery Powered`],
+            ["Manufacturer", `BEETEL TELETECH LIMITED`]
+           
+            
+
+        ],
+        bulletPoints: [
+            `3 AA batteries required`,
+            `6 one touch memory`,
+            `30 incoming phone memory`,
+            `5 outgoing phone memory`,
+            `Real-time clock with year, date and month display`,
+            `Pre dial and edit function`
+            
+        ],
+        imageLink: "https://m.media-amazon.com/images/I/812bIuvTRgL._SL1500_.jpg",
+        category: "te"
+    },
+
+    {
+        id: "1line",
+        name: "Panasonic Single Line KX-TS500MX Corded Telephone",
+        description: "Brand:Panasonic",
+        price: 000,
+        features: [
+            ["Product Dimensions", `‎‎‎‎17 x 9 x 19 cm; 699 Grams`],
+            ["Type", `Corded`],
+            ["Color",`White`],
+            ["Manufacturer", `Panasonic`],
+            ["Box Includes", `Handset, Base, Battery, Charger and User Guide`]
+            
+
+        ],
+        bulletPoints: [
+            `Corded phone with electronic volume control`,
+            `6 step electronic volume control`,
+            `Power Source: Land Line`,
+            `3 step ringer volume (off, low, high)`,
+            `Timed flash of 600ms`
+
+            
+        ],
+        imageLink: "https://m.media-amazon.com/images/I/31hR9MFUu7L.jpg",
+        category: "te"
+    },
+
+    {
+        id: "k260",
+        name: "Panasonic Corded phone Black",
+        description: "Brand:Panasonic",
+        price: 000,
+        features: [
+            ["Product Dimensions", `11.6 x 23 x 18.4 cm; 280 Grams`],
+            ["Type", `Corded`],
+            ["Number of Batteries",`2 AAA Batteries required`],
+            ["Manufacturer", `Panasonic`],
+            ["Box Includes", `Handset, Base, Battery, Charger and User Guide`]
+            
+
+        ],
+        bulletPoints: [
+            `Corded phone with caller ID and handsfree speakerphone`,
+            `Auto redial with 20 redial memory, 20 one touch and 10 speed dialer`,
+            `Ringer Indicator`
+
+
+            
+        ],
+        imageLink: "https://m.media-amazon.com/images/I/41xIc58UcSL.jpg",
+        category: "te"
+    },
+
+
+]
+
+
+function createProduct(id, name, price, imageLink){
+    return `
+    <div class="item col-md-4">
+        <a href="/product.html?pid=${id}">
+        <div class="featured-item">
+            <img src="${imageLink}" style="height: 200px !important; width: 200px !important" alt="">
+            <h4>${name}</h4>
+            <h6>Rs. ${price}</h6>
+        </div>
+        </a>
+    </div>
+    `
+}
+
+function createSinglePoint(point){
+    return `<li class="list-group-item">${point}</li>`
+}
+
+function createSingleTableRow(featureName, featureValue){
+    return `<tr><td>${featureName}</td><td>${featureValue}</td></tr>`
+}
+
+function createBulletPoint(pointsList){
+    var finalList = "";
+    for(var i = 0; i < pointsList.length; i++){
+        finalList+=createSinglePoint(pointsList[i]);
+    }
+    return finalList;
+}
+
+function createTableRows(features){
+    var innerHTML = "";
+    for(var i=0; i<features.length; i++){
+        innerHTML+=createSingleTableRow(features[i][0], features[i][1]);
+    }
+    return innerHTML;
+}
+
+function getSpecificProduct(id){
+    for(var i = 0; i < products.length; i++){
+        if(products[i].id == id){
+            return products[i]
+        }
+    }
+}
+
+const allProducts = document.getElementById("allproducts");
+if(allProducts){
+    const ptype = getUrlParameter("type")
+    var innerHTML = ""
+    for(var i=0; i<products.length; i++){
+        if(ptype){
+            if(ptype==products[i].category)
+            innerHTML += createProduct(products[i].id, products[i].name, products[i].price, products[i].imageLink);
+        }
+        else
+            innerHTML += createProduct(products[i].id, products[i].name, products[i].price, products[i].imageLink);
+    }
+    allProducts.innerHTML = innerHTML;
+}
+
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
+
+const pageBox = document.getElementById("pagebox");
+if(pageBox){
+    const productId = getUrlParameter("pid");
+    const product = getSpecificProduct(productId);
+    if(product){
+        document.getElementById("producttitle").innerHTML = product.name;
+        document.getElementById("productimage").setAttribute("src", product.imageLink);
+        document.getElementById("productname").innerHTML = product.name;
+        document.getElementById("productdescription").innerHTML = product.description;
+        document.getElementById("productprice").innerHTML = "Rs. " + product.price;
+        var points = createBulletPoint(product.bulletPoints);
+        document.getElementById("productpoints").innerHTML = `<ul class="list-group">${points}</ul>`
+        document.getElementById("productfeatures").innerHTML = createTableRows(product.features);
+    }else{
+        pageBox.innerHTML="<br><br><br><center><h3>Product Not Found!</h3></center>"
+    }
+}
