55e516adc4ef58459bb5be3a1a1010626ff837dd
aparnacl <18494870+aparnacl@users.noreply.github.com>
2021-09-27 13:19:17 +0530

    Add files via upload

--- assets/data/handler.js
+++ assets/data/handler.js
@@ -22,8 +22,8 @@ const products = [
 
     {
         id: "dome",
-        name: "Dome 2.4MP",
-        description: "2.4MP Full HD IR Cosmic Dome Camera - 30Mtr",
+        name: "CP Plus Cosmic HD 2.4MP IR Dome Camera 3.6 mm, 20 m, IR (CP-USC-DA24L2-0360)",
+        description: "2.4MP Full HD IR Cosmic Dome Camera - 20Mtr",
         price: 2100,
         features: [
             ["Image Sensor", `1/2.7" 2.4MP PS CMOS Image Sensor (6.858 centimeters)`],
@@ -33,7 +33,7 @@ const products = [
 
         ],
         bulletPoints: [
-            `1/2.7" 2.4MP PS CMOS Image Sensor (6.858 centimeters)`,
+            `Package Dimension: 11.18 x 11.18 x 10.92 cm; 259 Grams`,
             `Max 25/30fps@ 2.4MP`,
             `DWDR, Day/Night(ICR), 2D-DNR, AWB, AGC, BLC, HLC`,
             `3.6mm fixed lens (2.8mm, 6mm optional)`,
@@ -43,6 +43,63 @@ const products = [
         imageLink: "https://www.cpplusworld.com/prodassets/product/small/70bb461a-f28d-47c5-8107-2a0dd7ab7218.png",
         category: "cc"
     },
+
+    {
+        id: "guard+ dome",
+        name: "CP-GPC-D24L2-S",
+        description: "2.4MP Full HD IR Guard+ Dome Camera - 20 Mtr.",
+        price: 000,
+        features: [
+            ["Image Sensor", `1/2.9" 2.4MP CMOS Image Sensor (7.366 centimeters)`],
+            ["Minimum Illumination", `Color: 0.1 lux @ (F2.1, AGC ON), 0 lux with IR`],
+            ["BodyType", `Dome`],
+            ["Shutter Speed",`PAL: 1/25 s to 1/50,000 s; NTSC: 1/30 s to 1/50,000 s`],
+            ["White Balance",`ATW / MWB`],
+            ["LED Light",`Support Auto/Manual model,brightness can adjustable`],
+            ["No. of IR LEDs",`2 PCS 3600K soft warm light`],
+            ["Power Source",`12VDC ± 10%`],
+            ["Weatherproof Standard",`IP66`]
+        ],
+        bulletPoints: [
+            `1/2.9" 2.4MP CMOS Image Sensor (7.366 centimeters)`,
+            `Max 25/30fps@ 2.4MP`,
+            `DWDR, Day/Night(ICR), 2D/3D-DNR, AWB, AGC, BLC`,
+            `3.6mm lens (6mm optional)`,
+            `IR Range of 20 Mtr, IP66`
+        ],
+        imageLink: "https://www.cpplusworld.com/prodassets/product/small/cp-gpc-d24l2-s_2.jpg",
+        category: "cc"
+    },
+
+    {
+        id: "guard+ bullet",
+        name: "CP-GPC-T24PL2-S",
+        description: "2.4MP Full HD IR Guard+ Bullet Camera - 20 Mtr.",
+        price: 000,
+        features: [
+            ["Image Sensor", `1/2.9" 2.4MP CMOS Image Sensor (7.366 centimeters)`],
+            ["Minimum Illumination", `Color: 0.1 lux @ (F2.1, AGC ON), 0 lux with IR`],
+            ["BodyType", `Bullet`],
+            ["Shutter Speed",`PAL: 1/25 s to 1/50,000 s; NTSC: 1/30 s to 1/50,000 s`],
+            ["White Balance",`ATW / MWB`],
+            ["LED Light",`Support Auto/Manual model,brightness can adjustable`],
+            ["No. of IR LEDs",`2 PCS 3600K soft warm light`],
+            ["Power Source",`12VDC ± 10%`],
+            ["Weatherproof Standard",`IP66`]
+        ],
+        bulletPoints: [
+            `1/2.9" 2.4MP CMOS Image Sensor (7.366 centimeters)`,
+            `Max 25/30fps@ 2.4MP`,
+            `DWDR, Day/Night(ICR), 2D/3D-DNR, AWB, AGC, BLC`,
+            `3.6mm lens (6mm optional)`,
+            `IR Range of 20 Mtr, IP66`,
+            `Support low illumination,warm light day night full color`
+        ],
+        imageLink: "https://www.cpplusworld.com/prodassets/product/small/cp-gpc-t24pl2-s_2.jpg",
+        category: "cc"
+    },
+    
+
 
     {
         id: "c11",
@@ -386,6 +443,178 @@ const products = [
         imageLink: "https://m.media-amazon.com/images/I/41xIc58UcSL.jpg",
         category: "te"
     },
+
+    {
+        id: "8ch",
+        name: "CP-UVR-0801E1-HC",
+        description: "8 Ch. 1080P Lite Cosmic HD DVR",
+        price: 000,
+        features: [
+            ["Processor", `Embedded Processor`],
+            ["Operating System", `Embedded Linux`],
+            ["IP Camera input",`8+2,each channel up to 6MP`],
+            ["Two-way Talk", `Reuse audio in/out, RCA`],
+            ["User Interface", `GUI`],
+            ["Video Output",`1 HDMI, 1 VGA`],
+            ["Video Resolution",`1080N, 720P, 960H, D1, HD1, BCIF, CIF, QCIF`],
+            ["Video Recording",`Main Stream:1st channel 1080N/720P(1~25/30fps), others 1080N/720P(1~15fps); 960H/D1/HD1/BCIF/CIF/QCIF(1~25/30fps), Sub stream: CIF/QCIF (1~7fps)`],
+            ["Video Display Split",`1/4/8/9`],
+            ["Video Channel",`8 channel, BNC`],
+            ["Audio Output",`1 channel, RCA`],
+            ["Audio Channel",`1 channel, RCA`],
+            ["Motion Detection",`MD Zones: 396 (22 × 18)`],
+            ["Video Loss",`Supports`],
+            ["Smart Phone",`iPhone, iPad, Android Phone`],
+            ["Hard Disk",`1 SATA port, up to 6TB`],
+            ["Trigger Events",`Recording, PTZ, Tour, Video Push, Email, FTP, Snapshot, Buzzer and Screen Tips`],
+            ["Recording Mode",`Manual, Schedule (General, Continuous), MD (Video detection: Motion Detection, Video Loss, Tampering), Alarm, Stop`],
+            ["HDMI/VGA",`Simultaneous Video Output`],
+            ["Mobile Software",`iCMOB,gCMOB`],
+            ["CMS Software",`KVMS Pro`]
+            
+
+        ],
+        bulletPoints: [
+            `Auto Adaptive HDCVI/AHD/TVI/CVBS/IP signals`,
+            `Support upto 6MP IP Camera`,
+            `Max 40Mbps Incoming Bandwidth`,
+            `H.265+/ H.265 dual-stream video compression`,
+            `All channel 1080P Lite Non-Realtime Recording`,
+            `Support 1 SATA HDD up to 6TB, 2 USB2.0`
+
+
+
+            
+        ],
+        imageLink: "https://www.cpplusworld.com/prodassets/product/small/cp-uvr-0801e1-hc.jpg",
+        category: "dvr"
+    },
+
+    {
+        id: "16ch",
+        name: "CP-UVR-1601E1-HC",
+        description: "CP PLUS 16 CHANNEL DVR",
+        price: 000,
+        features: [
+            ["Processor", `Embedded Processor`],
+            ["Operating System", `Embedded Linux`],
+            ["IP Camera input",`8+2,each channel up to 6MP`],
+            ["Two-way Talk", `Reuse audio in/out, RCA`],
+            ["User Interface", `GUI`],
+            ["Video Output",`1 HDMI, 1 VGA`],
+            ["Video Resolution",`1080N, 720P, 960H, D1, HD1, BCIF, CIF, QCIF`],
+            ["Video Recording",`Main Stream:1st channel 1080N/720P(1~25/30fps), others 1080N/720P(1~15fps); 960H/D1/HD1/BCIF/CIF/QCIF(1~25/30fps), Sub stream: CIF/QCIF (1~7fps)`],
+            ["Video Display Split",`1/4/8/9`],
+            ["Video Channel",`8 channel, BNC`],
+            ["Audio Output",`1 channel, RCA`],
+            ["Audio Channel",`1 channel, RCA`],
+            ["Motion Detection",`MD Zones: 396 (22 × 18)`],
+            ["Video Loss",`Supports`],
+            ["Smart Phone",`iPhone, iPad, Android Phone`],
+            ["Hard Disk",`1 SATA port, up to 6TB`],
+            ["Trigger Events",`Recording, PTZ, Tour, Video Push, Email, FTP, Snapshot, Buzzer and Screen Tips`],
+            ["Recording Mode",`Manual, Schedule (General, Continuous), MD (Video detection: Motion Detection, Video Loss, Tampering), Alarm, Stop`],
+            ["HDMI/VGA",`Simultaneous Video Output`],
+            ["Mobile Software",`iCMOB,gCMOB`],
+            ["CMS Software",`KVMS Pro`]
+            
+
+        ],
+        bulletPoints: [
+            `Auto Adaptive HDCVI/AHD/TVI/CVBS/IP signals`,
+            `Support upto 2MP Camera`,
+            `Max 72Mbps Incoming Bandwidth`,
+            `H.265+/ H.265 dual-stream video compression`,
+            `All channel 1080P Lite Non-Realtime Recording`,
+            `Support 1 SATA HDD up to 6TB, 2 USB Ports(2.0)`
+
+
+
+            
+        ],
+        imageLink: "https://m.media-amazon.com/images/I/21PvjlcrEjL.jpg",
+        category: "dvr"
+    },
+
+    {
+        id: "ab",
+        name: "Main Stop® MXQ Pro 4K Android 10.0 TV Box",
+        description: " 2GB RAM/16GB ROM 64Bit Quad Core Processor",
+        price: 000,
+        features: [
+            ["Description",`Adopts the fast and powerful Android 10.0 operation system. Equipped with 2GB RAM 16GB ROM Allwinner H3 1.5GHz,Cortex A7,Quad Core and Mali - 450 GPU`],
+            ["Video Format",`1080P,4K x 2K, AVC, AVI, AVS, DAT, H.264, H.265, MKV, MOV, MP4, MPEG-4, MPEG1, MPEG2, MVC, RM, RMVB, VC-1, VOB, VP9 Profile-2, WMV;`],
+            ["Audio Format",`AAC, FLAC, OGG, RM, WMA; Photo Format: BMP, GIF, JPEG, JPG, PNG, TIFF; WIFI: 802.11b/g/n; Interface: AV, DC Power Port, HDMI, RJ45, TF card, USB2.0; Language: Multi-language; HDMI Version: 2.0; RJ45 Port Speed: 10 / 100M;`],
+            ["Network Connectivity",`Ethernet 10 / 100M and 2.4 GHz Wi-Fi, TF card (Up to 64GB), USB flash drive, etc.`]
+
+        ],
+        bulletPoints: [
+            `Item Dimensions: 21 x 16 x 6 Centimeters`,
+            `Connector Type: Ethernet, WIFI`,
+            `Brand: Main Stop`,
+            `Supported Internet Services: Browser, Amazon prime , you tube, whatsapp, hulu,zee5, sonyliv, kodi, gmail,facebook, jio tv, ip tv, gana,`,
+            `Control Method: Remote`            
+        ],
+        imageLink: "https://m.media-amazon.com/images/I/51GkRDLANmL._SL1000_.jpg",
+        category: "ab"
+    },
+
+    {
+        id: "vp",
+        name: "Villa Analog Kit (DS KIS204)",
+        description: "Brand:Hikvision",
+        price: 000,
+        features: [
+            ["Product Dimensions", `128 mm × 42 mm × 24.8 mm (5.0" × 1.7" × 0.9")`],
+            ["Audio Input", `Built-in Omnidirectional Microphone`],
+            ["Audio Output",`Built-in Loudspeaker`],
+            ["Audio Quality ", `Noise Suppression and Echo Cancellation`],
+            ["Four Wire Interface", `1 Four-Wire Interface (1-ch Video Input, 1-ch Audio Input/Output, 1-ch Power Supply, 1-ch Grounding)`],
+            ["Indoor Station Display Resolution",`800x480`],
+            ["Operation Method",`Physical Button`],
+            ["Indoor Station Dimension",`195.8 mm × 132.8 mm × 18.39 mm (7.7" × 5.2" ×0.7")`]
+            
+
+        ],
+        bulletPoints: [
+            `Camera: 1/4" CMOS`,
+            `Resolution:          720 × 576 `,
+            `Video Frame Rate: PAL: 25 fps`
+
+
+            
+        ],
+        imageLink: "https://www.hikvision.com/content/dam/hikvision/products/S000000001/S000000083/S000000088/S000000089/OFR000117/M000001576/images/DS-KIS204.png.thumb.1280.1280.png",
+        category: "vp"
+    },
+
+    {
+        id: "tr",
+        name: "COFE CF-4G903 Plug & Online 4G/4G+ WiFi Device)",
+        description: "Brand:COFE",
+        price: 000,
+
+        features: [
+           ["Power Source",`Micro USB`],
+           ["Router Type ",'4G Router'],
+           ["Password Security",`WPA(TKIP), WPA2(AES)`]
+
+        ],
+            bulletPoints: [
+            `Product Dimensions:‎12 x 12 x 12 cm; 100 Grams`,
+            `Batteries Required:No `,
+            `Wireless Type:‎2.4 GHz Radio Frequency `,
+            `Speaker Diameter: 12 Centimeters`,
+            `Connector Type: Wi-Fi,Ethernet`,
+            `Item Weight:100g`
+
+
+            
+        ],
+        imageLink: "https://m.media-amazon.com/images/I/71g3SaBoFIL._SL1500_.jpg",
+        category: "tr"
+    },
+
 
 
 ]
